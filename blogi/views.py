43c0f7da41e386327d2ce2571a0c30083dd68880
Sandro.Wüthrich <71585959+Sandrowue@users.noreply.github.com>
2023-02-06 14:24:14 +0200

    html texti

--- blogi/views.py
+++ blogi/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def postaukset(request):
+    return render(request, 'blogi/postauslista.html')
+    
