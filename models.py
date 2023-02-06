9eaa463bd1321e338e92f119e21320fafd5111dd
Sandro.Wüthrich <71585959+Sandrowue@users.noreply.github.com>
2023-02-06 11:55:58 +0200

    otsikko

--- models.py
+++ models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Postaus(models.Model):
     otsikko = models.CharField(max_length=200)
     teksti = models.TextField()
+
+    def __str__(self):
+        return self.otsikko
