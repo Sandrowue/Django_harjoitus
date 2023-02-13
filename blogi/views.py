ab03a08c3fb6e26279332ab504a902c8dcf0bab4
Sandro.Wüthrich <71585959+Sandrowue@users.noreply.github.com>
2023-02-13 14:57:01 +0200

    Toimiiko tämä?

--- blogi/views.py
+++ blogi/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import Postaus
+from django.urls import reverse
 # Create your views here.
 def postaukset(request):
     postaukset = Postaus.objects.all()
@@ -13,4 +14,23 @@ def nayta_postaus(request, id):
     return render(request, 'blogi/postaus.html', context)
     
 def uusi_postaus(request):
-    return render(request, "blogi/uusi_postaus.html")
+    if request.method == 'GET':
+        return render(request, "blogi/uusi_postaus.html")
+        # 1. Lomake näytetään ensimmäistä kertaan
+    elif request.method == 'POST':
+        # 2. Käyttäjä täytti ja lähetti lomakkeen
+
+        # luodaan lomakkeen tiedot POST-datasta
+        otsikko = request.POST['otsikko']
+        teksti = request.POST['teksti']
+        # Luodaan uusi Postaus objekti     
+        postaus = Postaus.objects.create(
+            otsikko=otsikko,
+            teksti=teksti,
+        )
+        # Muodostaa URL-osoite luotun Postaus-objektiin
+        url = reverse('nayta_postaus', args=(postaus.id,))
+        # Palautetaan uudelleenohjaus uudeen Postaus-objektin URL:iin
+        return redirect(url)
+
+    
