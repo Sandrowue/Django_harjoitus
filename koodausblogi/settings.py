e7681abfb559b0e719651b5f8b852400735a83a2
Sandro.Wüthrich <71585959+Sandrowue@users.noreply.github.com>
2023-03-13 10:43:19 +0200

    DATABASES muokattu

--- koodausblogi/settings.py
+++ koodausblogi/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,12 +22,13 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wbah-lp0%8lmuh!$b-wj+w6(&hagk)xl7$nykq$^fc&36e-8q)'
+SECRET_KEY = os.environ.get('SECRET_KEY', default= 'secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG', '').lower() not in ['0', 'no', 'false'])
 
-ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')    
+ALLOWED_HOSTS = [RENDER_EXTERNAL_HOSTNAME] if RENDER_EXTERNAL_HOSTNAME else []
 
 
 # Application definition
@@ -75,13 +76,10 @@ WSGI_APPLICATION = 'koodausblogi.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+sqlite_db = 'sqlite:///db.sqlite3'
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+    'default': dj_database_url.config(default=sqlite_db, conn_max_age=600),
+      }
 
 
 # Password validation
